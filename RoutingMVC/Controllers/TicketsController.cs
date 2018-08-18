2229bb56bb72af6087e3e59ccc052fe83d17280b
infocodify <bledinjo@hotmail.com>
2018-08-18 12:24:45 -0400

    Routing in asp.Net Core 1.1

---
+++ RoutingMVC/Controllers/TicketsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RoutingMVC.Models;
+
+namespace Routing_And_MVC.Controllers
+{
+    public class TicketsController : Controller
+    {
+        //Gredy Route
+        [HttpGet("/tix")]
+        public IActionResult Index()
+        {
+
+            var s = new Seats() { Location = "Number 10 Orchestra", Price = "300.00" };
+            return View(s);
+                
+                //Content( "Using IActionResult and return View");
+        }
+
+        [HttpGet("/tix2")]
+        public string Index2()
+        {
+            return "Using String and return";
+        }
+
+    }
+}
---
+++ RoutingMVC/Models/Seats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingMVC.Models
+{
+    //the Sets has a Location (number) and a Price
+    public class Seats
+    {
+        public string Location { get; set; }
+        public string Price { get; set; }
+    }
+}
